9962153a17262d01263811e802bed14dc68b9ff7
mimosafa <mimosafa@gmail.com>
2020-11-10 03:50:24 +0900

    support wp core install

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-wp core download --locale=$LOCALE
+# Download WordPress
+wp core download --locale=${LOCALE}
+if "${USE_SQLITE}"; then
+    cp wp-config-sample.php wp-config.php
+fi
+# Install WordPress
+wp core install --url=${SITE_URL} --title=${SITE_TITLE} --admin_user=${ADMIN_USER} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL}
